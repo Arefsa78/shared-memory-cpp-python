a9439ba4c6e83dc1f19d746df7ec0c4362d8eed1
arefsa <arefsayareh@gmail.com>
2022-02-28 10:35:30 +0330

    Python3.8 has bugs :(

--- main.cpp
+++ main.cpp
@@ -5,43 +5,69 @@
 #include <string>
 #include <iostream>
 
-#define SHARE_MEM_SIZE 1000
+#define SHARE_MEM_SIZE 20
 #define DATA_TYPE double
 
-int main() {
-   using namespace boost::interprocess;
-
-
-    struct shm_remove {
-        shm_remove() { shared_memory_object::remove("MySharedMemory"); }
-        ~shm_remove(){ shared_memory_object::remove("MySharedMemory"); }
-    } remover;
-
-    shared_memory_object shm (create_only, "MySharedMemory", read_write);
-    shm.truncate(sizeof(DATA_TYPE)*SHARE_MEM_SIZE);
-    //Map the whole shared memory in this process
-    mapped_region region(shm, read_write);
-
-    //Write all the memory to 1
-    std::memset(region.get_address(), 1, region.get_size());
-
-    DATA_TYPE* start = (DATA_TYPE*)(region.get_address());
-    DATA_TYPE* end = start + SHARE_MEM_SIZE;    
-    int i = 1;
-
-
-
-    for (DATA_TYPE* p = start; p < end; p++) { 
-        *p = (DATA_TYPE)i + (DATA_TYPE)i/10000;
-        i++;
+int main(int argc, char** argv) {
+    using namespace boost::interprocess;
+    std::cout << argv[0] << ", " << argv[1] << std::endl;
+    if (argc != 2){
+        std::cout << "s (send) or r (recieve)" << std::endl;
     }
+    else if (argv[1][0] == 'r'){
+        struct shm_remove {
+            shm_remove() { shared_memory_object::remove("MySharedMemory"); }
+            ~shm_remove(){ shared_memory_object::remove("MySharedMemory"); }
+        } remover;
 
-    for (DATA_TYPE* p = start; p < end; p++) { 
-        std::cout << *p << std::endl;
+        std::cout << "Opening" << std::endl;
+        shared_memory_object shm (open_only, "MySharedMemory", read_only);
+        std::cout << "Opened" << std::endl;
+        
+        //Map the whole shared memory in this process
+        mapped_region region(shm, read_only);
+
+        //Write all the memory to 1
+
+        DATA_TYPE* start = (DATA_TYPE*)(region.get_address());
+        DATA_TYPE* end = start + SHARE_MEM_SIZE;    
+        int i = 1;
+
+        for (DATA_TYPE* p = start; p < end; p++) { 
+            std::cout << *p << std::endl;
+        }
+
+        std::cout << "Done!" << std::endl;
     }
+    else if (argv[1][0] == 's'){
 
-    std::cout << "Done! now waiting..." << std::endl;
-    std::cout << sizeof(DATA_TYPE) << std::endl;
-    while(1);
+        shared_memory_object shm (create_only, "MySharedMemory", read_write);
+        shm.truncate(sizeof(DATA_TYPE)*SHARE_MEM_SIZE);
+        //Map the whole shared memory in this process
+        mapped_region region(shm, read_write);
+
+        //Write all the memory to 1
+        std::memset(region.get_address(), 1, region.get_size());
+
+        DATA_TYPE* start = (DATA_TYPE*)(region.get_address());
+        DATA_TYPE* end = start + SHARE_MEM_SIZE;    
+        int i = 1;
+
+        for (DATA_TYPE* p = start; p < end; p++) { 
+            *p = (DATA_TYPE)i + (DATA_TYPE)i/10000;
+            i++;
+        }
+
+        for (DATA_TYPE* p = start; p < end; p++) { 
+            std::cout << *p << std::endl;
+        }
+
+        std::cout << "Done! now waiting..." << std::endl;
+        std::cout << sizeof(DATA_TYPE) << std::endl;
+        while(1);
+    }
+    else {
+        std::cout << "s (send) or r (recieve)" << std::endl;
+    }
 
 }
--- main.py
+++ main.py
@@ -1,13 +1,36 @@
+from atexit import unregister
 from multiprocessing import shared_memory
 import numpy as np
-import mmap
-import ctypes
-import os
+import sys
 
-shm_a = shared_memory.SharedMemory(name="MySharedMemory", create=False, size=20)
-buffer = shm_a.buf
 
-arr = np.frombuffer(buffer.tobytes(), dtype="double")
-# lst = arr.tolist()
-print(arr)
-shm_a.unlink()
+if len(sys.argv) != 2:
+    print("s (send) or r (recieve)")
+elif sys.argv[1] == 's':
+    shm_b = shared_memory.SharedMemory(name="MySharedMemory", create=True, size=20*8)
+    
+    arr = np.ndarray((1,20), "double", shm_b.buf)
+    for i in range(20):
+        arr[0][i] = i + i/1000
+    
+    print(arr)
+    print("Done! waiting...")
+    while True:
+        pass
+    
+elif sys.argv[1] == 'r':
+    shm_a = shared_memory.SharedMemory(name="MySharedMemory", create=False)
+    # buffer = shm_a.buf
+
+    # arr = np.frombuffer(buffer.tobytes(), dtype="double")
+    # lst = arr.tolist()
+    # print(arr)
+    # np.delete(arr)
+    # del arr
+    # del buffer
+    shm_a.close()
+    # del shm_a
+    exit()
+else:
+    print("s (send) or r (recieve)")
+
